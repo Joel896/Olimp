ca1c6e9c92d5abda9b9646b419da4b38950f1014
Joel896 <86172298+Joel896@users.noreply.github.com>
2021-10-23 16:52:54 -0500

    Update TipoServicio.java

--- src/main/java/pe/edu/upc/spring/model/TipoServicio.java
+++ src/main/java/pe/edu/upc/spring/model/TipoServicio.java
@@ -2,7 +2,11 @@ package pe.edu.upc.spring.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity 
@@ -10,5 +14,12 @@ import javax.persistence.Table;
 public class TipoServicio implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idTipoServicio;
+	
+	@Column(name="nombreTipoServicio", length=30, nullable = false)
+	private String nombreTipoServicio;
 
 }
