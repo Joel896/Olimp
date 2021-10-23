3ed8e4a6e4e6d7afe664153c0c7b85ee8e1c9acc
david1817 <david.1817@outlook.com>
2021-10-22 20:14:40 -0500

    update 22/10
    update

---
+++ src/main/java/pe/edu/upc/spring/model/Tarifario.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.spring.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name="Tarifario")
+public class Tarifario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idTarifario;
+	
+	@ManyToOne
+	@JoinColumn(name="idTipoVehiculo", nullable=false)
+	private TipoVehiculo tipoVehiculo;
+	
+	@ManyToOne
+	@JoinColumn(name="idServicio", nullable=false)
+	private Servicio servicio;
+	
+	@Column(name="Precio", length=60, nullable=false)
+	private String precio;
+	
+}
