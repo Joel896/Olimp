d12558dc7291bde76841497921039d4dac25ddc1
Joel896 <86172298+Joel896@users.noreply.github.com>
2021-10-23 18:21:51 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/spring/repository/IServicioRepository.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.spring.model.Servicio;
+
+@Repository
+public interface IServicioRepository extends JpaRepository<Servicio, Integer>{
+	@Query("from Servicio r where r.nombreServicio like %:nombreServicio%")
+	List<Servicio> buscarNombre(@Param("nombreServicio")String nombreServicio);
+}
