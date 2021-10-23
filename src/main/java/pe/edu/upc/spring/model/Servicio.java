69d3d5aeb56a4cfdfe21c1f2199e99dda0dab52e
Joel896 <86172298+Joel896@users.noreply.github.com>
2021-10-23 16:50:49 -0500

    Update Servicio.java

--- src/main/java/pe/edu/upc/spring/model/Servicio.java
+++ src/main/java/pe/edu/upc/spring/model/Servicio.java
@@ -2,7 +2,9 @@ package pe.edu.upc.spring.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity 
@@ -10,5 +12,23 @@ import javax.persistence.Table;
 public class Servicio implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idServicio;
+	
+	@ManyToOne
+	@JoinColumn(name="idTipoServicio", nullable=false)
+	private TipoServicio tipoServicio;
+	
+	@ManyToOne
+	@JoinColumn(name="idSucursal", nullable=false)
+	private Sucursal sucursal;
+	
+	@Column(name="Nombre", length=60, nullable=false)
+	private String nombre;
+	
+	@Column(name="Descripcion", length=60, nullable=false)
+	private String descripcion;
 
 }
