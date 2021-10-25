c081c26b471db75f40e5a36d4d022ffbfc48acbb
david1817 <david.1817@outlook.com>
2021-10-24 23:06:39 -0500

    update 24/10

---
+++ src/main/java/pe/edu/upc/spring/controller/TarifarioController.java
@@ -0,0 +1,157 @@
+package pe.edu.upc.spring.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sun.el.parser.ParseException;
+
+
+import pe.edu.upc.spring.model.Tarifario;
+import pe.edu.upc.spring.model.TipoVehiculo;
+import pe.edu.upc.spring.model.Servicio;
+import pe.edu.upc.spring.service.ITarifarioService;
+import pe.edu.upc.spring.service.ITipoVehiculoService;
+import pe.edu.upc.spring.service.IServicioService;
+
+@Controller
+@RequestMapping("/(tarifario)")
+public class TarifarioController {
+	@Autowired
+	private IServicioService sService;
+	
+	@Autowired
+	private ITipoVehiculoService tvService;
+	
+	@Autowired
+	private ITarifarioService tService;	
+	
+	@RequestMapping("/bienvenido")
+	public String irPaginaBienvenida() {
+		return "bienvenido";
+	}
+		
+	@RequestMapping("/")
+	public String irPaginaListadoMascotas(Map<String, Object> model) {
+		model.put("listaMascotas", tService.Listar());
+		return "listTarifario";
+	}
+	
+	@RequestMapping("/irRegistrar")
+	public String irPaginaRegistrar(Model model) {
+		
+		model.addAttribute("tipoVehiculo", new TipoVehiculo());
+		model.addAttribute("servicio", new Servicio());
+		model.addAttribute("tarifario", new Tarifario());
+		model.addAttribute("listaTipoVehiculo", tvService.listar());
+		model.addAttribute("listaServicio", sService.listar());		
+		
+		return "tarifario";
+	}
+	
+	@RequestMapping("/registrar")
+	public String Registrar(@ModelAttribute Tarifario objTarifario, BindingResult binRes, Model model)
+			throws ParseException
+	{
+		if (binRes.hasErrors()) 
+			{
+				model.addAttribute("listaTipoVehiculo", tvService.listar());
+				model.addAttribute("listaServicio", sService.listar());			
+				return "tarifario";
+			}
+		else {
+			boolean flag = tService.Registrar(objTarifario);
+			if (flag)
+				return "redirect:/tarifario/listar";
+			else {
+				model.addAttribute("mensaje", "Ocurrio un error");
+				return "redirect:/tarifario/irRegistrar";
+			}
+		}
+	}
+	
+	
+	@RequestMapping("/modificar/{id}")
+	public String Modificar(@PathVariable int id, Model model, RedirectAttributes objRedir)
+		throws ParseException 
+	{
+		Optional<Tarifario> objTarifario = tService.BuscarId(id);
+		if (objTarifario == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/tarifario/listar";
+		}
+		else {
+			model.addAttribute("listaTipoVehiculo", tvService.listar());
+			model.addAttribute("listaServicio", sService.listar());			
+					
+			if (objTarifario.isPresent())
+				objTarifario.ifPresent(o -> model.addAttribute("tarifario", o));
+			
+			return "tarifario";
+		}
+	}
+	
+	@RequestMapping("/eliminar")
+	public String Eliminar(Map<String, Object> model, @RequestParam(value="id") Integer id) {
+		try {
+			if (id!=null && id>0) {
+				tService.Eliminar(id);
+				model.put("listaTarifario", tService.Listar());
+			}
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			model.put("mensaje","Ocurrio un roche");
+			model.put("listaTarifario", tService.Listar());
+			
+		}
+		return "listTarifario";
+	}
+	
+	@RequestMapping("/listar")
+	public String Listar(Map<String, Object> model) {
+		model.put("listaTarifario", tService.Listar());
+		return "listTarifario";
+	}		
+	
+	@RequestMapping("/listarId")
+	public String ListarId(Map<String, Object> model, @ModelAttribute Tarifario tarifario) 
+	throws ParseException
+	{
+		tService.ListarId(tarifario.getIdTarifario());
+		return "listTarifario";
+	}	
+	
+	@RequestMapping("/irBuscar")
+	public String irBuscar(Model model) 
+	{
+		model.addAttribute("tarifario", new Tarifario());
+		return "buscar";
+	}	
+	
+	@RequestMapping("/buscar")
+	public String buscar(Map<String, Object> model, @ModelAttribute Tarifario tarifario)
+			throws ParseException
+	{
+		List<Tarifario> listaTarifario;
+		if (listaTarifario.isEmpty()) {
+			listaTarifario = tService.ListarPorNombreDeServicio(tarifario.getServicio().getNombreServicio());
+		}
+		if (listaTarifario.isEmpty()) {
+			listaTarifario = tService.ListarPorIdDeServicio(tarifario.getServicio().getIdServicio());
+		}
+	}
+		model.put("listaTarifario", listaTarifario);
+		return "buscar";
+	}
+}
--- src/main/java/pe/edu/upc/spring/model/Tarifario.java
+++ src/main/java/pe/edu/upc/spring/model/Tarifario.java
@@ -31,5 +31,48 @@ public class Tarifario implements Serializable{
 	
 	@Column(name="precioTarifario", length=60, nullable=false)
 	private String precio;
-	
+
+	public Tarifario() {
+		super();
+	}
+
+	public Tarifario(int idTarifario, TipoVehiculo tipoVehiculo, Servicio servicio, String precio) {
+		super();
+		this.idTarifario = idTarifario;
+		this.tipoVehiculo = tipoVehiculo;
+		this.servicio = servicio;
+		this.precio = precio;
+	}
+
+	public int getIdTarifario() {
+		return idTarifario;
+	}
+
+	public void setIdTarifario(int idTarifario) {
+		this.idTarifario = idTarifario;
+	}
+
+	public TipoVehiculo getTipoVehiculo() {
+		return tipoVehiculo;
+	}
+
+	public void setTipoVehiculo(TipoVehiculo tipoVehiculo) {
+		this.tipoVehiculo = tipoVehiculo;
+	}
+
+	public Servicio getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
 }
