948dd25ac8b1098d6005f002107262ccc5d32847
RonnieRantes <rantesroni1@gmail.com>
2021-11-09 03:15:08 -0500

    update

--- src/main/java/pe/edu/upc/spring/controller/VisualizarController.java
+++ src/main/java/pe/edu/upc/spring/controller/VisualizarController.java
@@ -11,10 +11,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sun.el.parser.ParseException;
 
+import pe.edu.upc.spring.model.Servicio;
 import pe.edu.upc.spring.model.Sucursal;
 import pe.edu.upc.spring.service.ICalificacionService;
+import pe.edu.upc.spring.service.IImagenService;
 import pe.edu.upc.spring.service.IServicioService;
 import pe.edu.upc.spring.service.ISucursalService;
+import pe.edu.upc.spring.service.ITarifaService;
 
 @Controller
 @RequestMapping("/visualizar")
@@ -25,6 +28,10 @@ public class VisualizarController {
 	public ISucursalService suService;
 	@Autowired
 	public ICalificacionService cService;
+	@Autowired
+	public ITarifaService tService;
+	@Autowired
+	public IImagenService iService;
 	
 	@RequestMapping("/sucursal/{id}")
 	public String irVisualizarSucursal(@PathVariable int id, Model model, RedirectAttributes objRedir) throws ParseException {
@@ -37,19 +44,39 @@ public class VisualizarController {
 			if (objSucursal.isPresent()) {
 				objSucursal.ifPresent(o -> model.addAttribute("sucursal", o));
 				model.addAttribute("listaServicios", sService.buscarSucursal(id));
+				//Calificaciones
 				model.addAttribute("total", cService.contarCalificaciones("sucursal", id, -1));			
-				model.addAttribute("media", cService.contarCalificaciones("sucursal", id, 0));			
-				model.addAttribute("cal1", cService.contarCalificaciones("sucursal", id, 1));	
-				model.addAttribute("cal2", cService.contarCalificaciones("sucursal", id, 2));			
-				model.addAttribute("cal3", cService.contarCalificaciones("sucursal", id, 3));			
-				model.addAttribute("cal4", cService.contarCalificaciones("sucursal", id, 4));			
-				model.addAttribute("cal5", cService.contarCalificaciones("sucursal", id, 5));	
+				model.addAttribute("media", cService.contarCalificaciones("sucursal", id, 0));
+				for(int i = 1; i <= 5; i++) model.addAttribute("cal"+i,cService.contarCalificaciones("sucursal", id, i));	
 			}
 			return "verSucursal";
 		}
 	}
 	@RequestMapping("/servicio/{id}")
-	public String irVisualizarServicio(@PathVariable int id, Model model) throws ParseException {
-		return "verSucursal";
+	public String irVisualizarServicio(@PathVariable int id, Model model, RedirectAttributes objRedir) throws ParseException {
+		Optional<Servicio> objServicio = sService.buscarId(id);
+		if (objServicio == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/busqueda/servicio/";
+		}
+		else {		
+			if (objServicio.isPresent()) {
+				objServicio.ifPresent(o -> model.addAttribute("servicio", o));
+				model.addAttribute("listaServicios", sService.buscarSucursal(id));
+				model.addAttribute("listaTarifas", tService.buscarServicio(id));
+				model.addAttribute("listaImagenes", iService.buscarServicio(id));
+				//Calificaciones
+				model.addAttribute("listaCalificaciones", cService.buscarServicio(id));
+				model.addAttribute("total", cService.contarCalificaciones("sucursal", id, -1));			
+				model.addAttribute("media", cService.contarCalificaciones("servicio", id, 0));			
+				for(int i = 1; i <= 5; i++) model.addAttribute("cal"+i,cService.contarCalificaciones("servicio", id, i));	
+			}
+			return "verServicio";
+		}
+	}
+	
+	@RequestMapping("/imagen/")
+	public String irVisualizarImagen() throws ParseException{
+		return "verImagen";
 	}
 }
